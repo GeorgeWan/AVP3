8f74b3dd7d69f318b693d0f3e067e092dc92f5a6
George <ywan2@uco.edu>
2016-03-06 11:26:23 -0600

    first commit by George

---
+++ project3/src/project3/wwwroot/js/app.js
@@ -0,0 +1,29 @@
+﻿var myApp = angular.module("myApp", ['ngRoute']);
+
+
+myApp.config(function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'pages/first.html',
+        controller: 'firstController'
+        })
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+})
+
+
+myApp.controller("mainController", ["$scope", "$log",function ($scope, $log) {
+    $scope.name = 'Family';
+   
+}]);
+
+myApp.controller("firstController", ["$scope", "$log", function ($scope, $log) {
+    $scope.name = 'George';
+}]);
+
+
+myApp.controller("secondController", ["$scope", "$log", function ($scope, $log) {
+    $scope.name = 'Rachel';
+}]);
